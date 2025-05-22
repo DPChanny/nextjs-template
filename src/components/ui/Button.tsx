fefd7dd1df1fe21edecc1265ccf93c032ad20c04
DPChanny <kyoungchan0516@gmail.com>
2025-05-23 05:19:59 +0900

    adjustment

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -3,8 +3,6 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 import styles from "@/styles/ui/Button.module.css";
 
-console.log(styles);
-
 const buttonVariants = cva(styles.button, {
   variants: {
     intent: {
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,11 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: ["./src/styles/**/*.{css,scss,sass}"],
+  content: [
+    "./src/styles/**/*.{js, jsx, ts, tsx, css, scss, sass}",
+    "./src/components/**/*.{js, jsx, ts, tsx, css, scss, sass}",
+    "./src/app/**/*.{js, jsx, ts, tsx, css, scss, sass}",
+  ],
   theme: {
     extend: {},
   },
