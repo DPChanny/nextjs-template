69b57c4dfe0451e8983bbe749486407153628b65
DPChanny <kyoungchan0516@gmail.com>
2025-05-23 16:29:02 +0900

    tailwind rule

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/styles/**/*.{js, jsx, ts, tsx, css, scss, sass}",
-    "./src/components/**/*.{js, jsx, ts, tsx, css, scss, sass}",
-    "./src/app/**/*.{js, jsx, ts, tsx, css, scss, sass}",
-  ],
+  content: ["./src/components/**/*.{ts, tsx}", "./src/app/**/*.{ts, tsx}"],
   theme: {
     extend: {},
   },
